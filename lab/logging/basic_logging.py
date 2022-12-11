4cc6e35dc8d9151956d42216230b9179b9becb60
Peng <pfc5098@gmail.com>
2022-12-10 21:47:24 -0800

    improve the template

--- lab/logging/basic_logging.py
+++ lab/logging/basic_logging.py
@@ -1,6 +1,21 @@
-import logging 
-logging.basicConfig(filename='basics.log', 
-                    level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
+import os
+import logging
 
-logging.info('hello A')
-logging.info('hello B')
+log_file_name = 'log_file.log'
+if os.path.exists(log_file_name):
+    os.remove(log_file_name)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S',
+    handlers=[logging.FileHandler(log_file_name), logging.StreamHandler()]
+)
+
+import time
+
+logging.info('Preparing data.')
+time.sleep(1)
+logging.info('Fitting model.')
+time.sleep(1)
+logging.info('Making prediction.')
+time.sleep(1)
